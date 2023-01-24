f7a11622e914f6663dea1240e60ef96fc9c94d5d
karamyf <karam_youssef@emsi-edu.ma>
2023-01-24 21:14:36 +0100

    dpl_error_fix

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 //static files
 app.use(express.static(path.join(__dirname, './client/build')))
 app.get('*' , function(req,res){ 
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 //middlewares
