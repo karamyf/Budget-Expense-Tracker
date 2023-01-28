e621eae538711ab6572f737aa1dc64727007b071
karamyf <karam_youssef@emsi-edu.ma>
2023-01-28 20:16:27 +0100

    rmv unused elements

--- frontend/src/components/Favorites.js
+++ frontend/src/components/Favorites.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 
 const Favorites = ({ allTransaction }) => {
     const [transactions, setTransactions] = useState([]);
