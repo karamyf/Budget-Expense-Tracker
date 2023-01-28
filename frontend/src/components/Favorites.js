bf24034a288e088dae3ef0a79210a30280d78d13
karamyf <karam_youssef@emsi-edu.ma>
2023-01-28 19:28:55 +0100

    delete from fav transaction

--- frontend/src/components/Favorites.js
+++ frontend/src/components/Favorites.js
@@ -4,48 +4,65 @@ import axios from "axios";
 const Favorites = ({ allTransaction }) => {
     const [transactions, setTransactions] = useState([]);
     const [loading, setLoading] = useState(false);
-  
+
     useEffect(() => {
-      setLoading(true);
-      // Retrieve the transactions list from local storage
-      const transactionsList = JSON.parse(localStorage.getItem("transactionsList")) || [];
-      setTransactions(transactionsList);
-      setLoading(false);
+        setLoading(true);
+        // Retrieve the transactions list from local storage
+        const transactionsList = JSON.parse(localStorage.getItem("transactionsList")) || [];
+        setTransactions(transactionsList);
+        setLoading(false);
     }, []);
-  
+
+    // Function to delete a transaction
+    const handleDelete = (transaction) => {
+        // Delete the transaction from the state
+        setTransactions(transactions.filter(t => t._id !== transaction._id));
+        // Update the transactions list in local storage
+        localStorage.setItem("transactionsList", JSON.stringify(transactions.filter(t => t._id !== transaction._id)));
+    }
+
     return (
-      <div>
-        {loading ? (
-          <div>Loading...</div>
-        ) : (
-          <div>
-            {transactions.length > 0 ? (
-              <table>
-                <thead>
-                  <tr>
-                    <th>Name</th>
-                    <th>Amount</th>
-                    <th>Date</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {transactions.map((transaction) => (
-                    <tr key={transaction._id}>
-                      <td>{transaction.name}</td>
-                      <td>{transaction.amount}</td>
-                      <td>{transaction.date}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
+        <div>
+            {loading ? (
+                <div>Loading...</div>
             ) : (
-              <div>You have no favorite transactions.</div>
+                <div>
+                    {transactions.length > 0 ? (
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Date</th>
+                                    <th>Amount</th>
+                                    <th>Description</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {transactions.map((transaction) => {
+                                    const date = new Date(transaction.date);
+                                    const formattedDate = date.toISOString().slice(0, 10);
+                                    return (
+                                        <tr key={transaction._id}>
+                                            <td>{formattedDate}</td>
+                                            <td>{transaction.amount}</td>
+                                            <td>{transaction.description}</td>
+                                            <td>
+                                                <button onClick={() => handleDelete(transaction)}>Delete</button>
+                                            </td>
+                                        </tr>
+                                    );
+                                })}
+                            </tbody>
+
+                        </table>
+                    ) : (
+                        <div>You have no favorite transactions.</div>
+                    )}
+                </div>
             )}
-          </div>
-        )}
-      </div>
+        </div>
     );
-  };
-  
+};
 
 export default Favorites;
+
